1fb656cafea20ddb7f28fc1d83ae886d3e8921a4
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 23:16:07 +0600

    auto: 5/29/2025, 11:16:07 PM

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -57,7 +57,7 @@ const Register = () => {
                 <button className="btn btn-neutral mt-4">Register</button>
               </fieldset>
             </form>
-            
+            <
           </div>
         </div>
       </div>
