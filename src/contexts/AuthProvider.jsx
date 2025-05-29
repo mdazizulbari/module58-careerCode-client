79f9920d2e9867df674e183dcd5e941f824c4689
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 20:32:58 +0600

    auto: 5/29/2025, 8:32:58 PM

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -3,7 +3,7 @@ import { AuthContext } from './AuthContext';
 
 const AuthProvider = ({children}) => {
 const createUser=(email,password){
-    return createUserWithEmailAndPassword(auth email,password)
+    return createUserWithEmailAndPassword(auth, email,password)
 }
 
     const authInfo={
