9c2df35b380adf794a51e1656b7dd0718ee17a50
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 21:05:28 +0600

    auto: 5/29/2025, 9:05:28 PM

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
@@ -11,12 +14,12 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const signInUser=(email,password)=>{
-    setLoading(true)
-    return signInWithEmailAndPassword(auth,email,password)
-  }
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-  const authInfo = { loading, createUser };
+  const authInfo = { loading, createUser, signInUser };
 
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
