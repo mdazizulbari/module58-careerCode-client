77156ce5fe0fe1ae22cb92202b5eec04ac5edcd8
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 22:57:29 +0600

    auto: 5/29/2025, 10:57:28 PM

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -5,7 +5,12 @@ const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className=""></div>
+        <div className="">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+        </div>
         <div>
           <motion.h1
             initial={{ rotate: -180 }}
