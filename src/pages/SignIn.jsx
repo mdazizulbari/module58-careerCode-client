703ea76226fab09de56ebec009bc745a046e6869
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 20:59:26 +0600

    auto: 5/29/2025, 8:59:26 PM

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const SignIn = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <Lottie
+            style={{ width: "200px" }}
+            loop={true}
+            animationData={registerLottie}
+          ></Lottie>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+            <form onSubmit={handleResister}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
