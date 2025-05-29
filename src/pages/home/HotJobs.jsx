bb43e0133a3e16303ca7dfe19878bab31bdbfca3
smabari <mdazizulbarishafin@gmail.com>
2025-05-30 02:12:04 +0600

    auto: 5/30/2025, 2:12:04 AM

--- src/pages/home/HotJobs.jsx
+++ src/pages/home/HotJobs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HotJobs = () => {
-    const jobsPromise =fetch('http://localhost:5000/jobs').then
+    const jobsPromise =fetch('http://localhost:5000/jobs').then(res=>re)
 
 
     return (
