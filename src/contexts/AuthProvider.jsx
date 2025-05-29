db9f808cd1680accaf8345b55e5ccb9d5868609a
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 20:44:21 +0600

    auto: 5/29/2025, 8:44:21 PM

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AuthContext } from "./AuthContext";
+import { createUserWithEmailAndPassword } from "firebase/auth/web-extension";
 
 const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
