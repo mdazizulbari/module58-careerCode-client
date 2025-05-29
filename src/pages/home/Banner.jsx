cb2fadcbfd58c7934070e5dc62288343c5e615c2
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 23:00:41 +0600

    auto: 5/29/2025, 11:00:41 PM

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className="flex-1">
           <img
             src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-4xl"
+            className="max-w-sm rounded-4xl "
           />
         </div>
         <div className="flex-1">
