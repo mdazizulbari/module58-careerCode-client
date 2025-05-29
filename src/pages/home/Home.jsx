ca9b2d3364f7955a2b874fc74819ab2bb63766bc
smabari <mdazizulbarishafin@gmail.com>
2025-05-30 02:14:13 +0600

    auto: 5/30/2025, 2:14:13 AM

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,9 @@ import Banner from "./Banner";
 import HotJobs from "./HotJobs";
 
 const Home = () => {
+  const jobsPromise = fetch("http://localhost:5000/jobs")
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
   return (
     <div>
       <Banner />
