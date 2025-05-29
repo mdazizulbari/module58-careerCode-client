a5d494f16616fc40a1bb20b91e320752f82f503c
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 20:53:42 +0600

    auto: 5/29/2025, 8:53:42 PM

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITEapiKey,
+  apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.authDomain,
   projectId: import.meta.env.projectId,
   storageBucket: import.meta.env.storageBucket,
