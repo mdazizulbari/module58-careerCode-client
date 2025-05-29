c01c9c53b24880c064761c1d92aa0d2b353daf6c
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 21:09:11 +0600

    auto: 5/29/2025, 9:09:11 PM

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -25,7 +25,7 @@ const AuthProvider = ({ children }) => {
     const unSubscribe= onAuthStateChanged(auth,currentUser=>{
       setUser(currentUser)
       setLoading(false)
-      console.log('')
+      console.log('user in t')
     })
   })
 
