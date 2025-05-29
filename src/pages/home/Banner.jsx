4587cd4fa4e4daa76d2809520fd1f1ac9cd12bf3
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 22:57:40 +0600

    auto: 5/29/2025, 10:57:40 PM

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="">
+        <div className="flex">
           <img
             src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
             className="max-w-sm rounded-lg shadow-2xl"
