ef1d16687466f687178106af6b81b2ff513fa31c
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 20:53:52 +0600

    auto: 5/29/2025, 8:53:52 PM

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.storageBucket,
+  storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.messagingSenderId,
   appId: import.meta.env.appId,
 };
