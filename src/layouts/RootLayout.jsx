f586a3d1d564c87e369860dda4bb6325294aa849
smabari <mdazizulbarishafin@gmail.com>
2025-05-29 17:16:18 +0600

    initial setup

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router";
+import NavBar from "../pages/shared/NavBar";
+import Footer from "../pages/shared/Footer";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default RootLayout;
