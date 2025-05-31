fa6ade95d0b4ec06fb0e44b5d3db010f89bc2ade
smabari <mdazizulbarishafin@gmail.com>
2025-06-01 00:45:35 +0600

    Add JobApply component with form for job application links and implement useAuth hook

--- src/pages/JobApply.jsx
+++ src/pages/JobApply.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { useParams } from "react-router";
+import useAuth from "../hooks/useAuth";
 
 const JobApply = () => {
+  const { id: jobId } = useParams();
+  const { user } = useAuth();
+  console.log(jobId, user);
+
+  const handleApply = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const linkedIn=form.linkedIn.value
+  };
+
   return (
     <div>
-      <h2>Apply for Job</h2>
+      <h2 className="text-4xl">Apply for Job</h2>
+      <form onSubmit={handleApply} action="">
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <label className="label">LinkedIn Link</label>
+          <input
+            type="url"
+            className="input"
+            name="linkedIn"
+            placeholder="LinkedIn Profile Link"
+          />
+
+          <label className="label">GitHub Link</label>
+          <input
+            type="url"
+            name="github"
+            className="input"
+            placeholder="GitHub Link"
+          />
+
+          <label className="label">Resume Link</label>
+          <input
+            type="url"
+            name="resume"
+            className="input"
+            placeholder="Resume Link"
+          />
+
+          <input type="submit" value="Apply" className="btn" />
+        </fieldset>
+      </form>
     </div>
   );
 };
