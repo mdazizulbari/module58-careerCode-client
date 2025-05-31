7804353bc8c9f491a86f5b78bc7ecf5ed967f258
smabari <mdazizulbarishafin@gmail.com>
2025-05-31 22:26:45 +0600

    Add Link import to JobDetails component for navigation

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLoaderData } from "react-router";
+import { Link, useLoaderData } from "react-router";
 
 const JobDetails = () => {
   const { title, _id, company } = useLoaderData();
