59db53d1cfc2956c8099f9725afed2cbce79b606
smabari <mdazizulbarishafin@gmail.com>
2025-06-01 15:12:38 +0600

    Remove unused footer from ApplicationList component

--- src/pages/myApplications/ApplicationList.jsx
+++ src/pages/myApplications/ApplicationList.jsx
@@ -164,16 +164,6 @@ const ApplicationList = ({ myApplicationsPromise }) => {
               </th>
             </tr>
           </tbody>
-          {/* foot */}
-          <tfoot>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
-            </tr>
-          </tfoot>
         </table>
       </div>
     </div>
