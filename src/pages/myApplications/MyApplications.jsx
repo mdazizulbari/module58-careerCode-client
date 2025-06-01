a35b9f47b1574418ec63fb636e63db0e961e6396
smabari <mdazizulbarishafin@gmail.com>
2025-06-01 14:42:19 +0600

    Add myApplicationsPromise function to fetch user applications and pass it to ApplicationList

--- src/pages/myApplications/MyApplications.jsx
+++ src/pages/myApplications/MyApplications.jsx
@@ -1,13 +1,26 @@
 import React, { Suspense } from "react";
 import ApplicationStats from "./ApplicationStats";
 import ApplicationList from "./ApplicationList";
+import useAuth from "../../hooks/useAuth";
+
+const myApplicationsPromise = (email) => {
+  return fetch(`http://localhost:5000/applications?email=${email}`)
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 const MyApplications = () => {
+  const { user } = useAuth;
+
   return (
     <div>
       <ApplicationStats />
       <Suspense fallback={"Loading your Applications..."}>
-        <ApplicationList />
+        <ApplicationList
+          myApplicationsPromise={myApplicationsPromise(user.email)}
+        />
       </Suspense>
     </div>
   );
