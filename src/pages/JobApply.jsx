cd1cc3463ceb339913e7b8a01ad6c0ccf847137d
smabari <mdazizulbarishafin@gmail.com>
2025-06-01 10:05:00 +0600

    Add axios dependency and implement job application form submission in JobApply component

--- src/pages/JobApply.jsx
+++ src/pages/JobApply.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router";
 import useAuth from "../hooks/useAuth";
+import axios from "axios";
 
 const JobApply = () => {
   const { id: jobId } = useParams();
@@ -15,45 +16,59 @@ const JobApply = () => {
     const resume = form.resume.value;
     // console.log(linkedIn, github, resume);
     const application = {
-      jobId, applicant: user.email,
+      jobId,
+      applicant: user.email,
+      linkedIn,
+      github,
+      resume,
+    };
+
+    axios
+      .post("", application)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return (
+      <div>
+        <h2 className="text-4xl">
+          Apply for this <Link to={`/jobs/${jobId}`}>Job</Link>
+        </h2>
+        <form onSubmit={handleApply} action="">
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <label className="label">LinkedIn Link</label>
+            <input
+              type="url"
+              className="input"
+              name="linkedIn"
+              placeholder="LinkedIn Profile Link"
+            />
+
+            <label className="label">GitHub Link</label>
+            <input
+              type="url"
+              name="github"
+              className="input"
+              placeholder="GitHub Link"
+            />
+
+            <label className="label">Resume Link</label>
+            <input
+              type="url"
+              name="resume"
+              className="input"
+              placeholder="Resume Link"
+            />
+
+            <input type="submit" value="Apply" className="btn" />
+          </fieldset>
+        </form>
+      </div>
+    );
   };
-
-  return (
-    <div>
-      <h2 className="text-4xl">
-        Apply for this <Link to={`/jobs/${jobId}`}>Job</Link>
-      </h2>
-      <form onSubmit={handleApply} action="">
-        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          <label className="label">LinkedIn Link</label>
-          <input
-            type="url"
-            className="input"
-            name="linkedIn"
-            placeholder="LinkedIn Profile Link"
-          />
-
-          <label className="label">GitHub Link</label>
-          <input
-            type="url"
-            name="github"
-            className="input"
-            placeholder="GitHub Link"
-          />
-
-          <label className="label">Resume Link</label>
-          <input
-            type="url"
-            name="resume"
-            className="input"
-            placeholder="Resume Link"
-          />
-
-          <input type="submit" value="Apply" className="btn" />
-        </fieldset>
-      </form>
-    </div>
-  );
 };
 
 export default JobApply;
