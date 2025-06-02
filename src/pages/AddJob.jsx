440b35c61adb5f1fb27fc56b8598c19b8a2a18ee
smabari <mdazizulbarishafin@gmail.com>
2025-06-02 15:10:45 +0600

    Add job type, category, application deadline, and salary range fields to AddJob component

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -42,6 +42,109 @@ const AddJob = () => {
 
           <button className="btn btn-neutral mt-4">Login</button>
         </fieldset>
+
+        {/* job type */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Job Type</legend>
+
+          <div className="filter">
+            <input
+              className="btn filter-reset"
+              type="radio"
+              name="jobType"
+              aria-label="All"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="On-Site"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="Remote"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="Hybrid"
+            />
+          </div>
+        </fieldset>
+
+        {/* job category */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Job Category</legend>
+
+          <select defaultValue="Job Category" className="select">
+            <option disabled={true}>Job Category</option>
+            <option>Engineering</option>
+            <option>Marketing</option>
+            <option>Finance</option>
+          </select>
+        </fieldset>
+
+        {/* application deadline */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Application Deadline</legend>
+
+          <input type="date" className="input" />
+        </fieldset>
+
+        {/* Salary Range */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Salary Range</legend>
+
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <div className="">
+              <label className="label">Minimum Salary</label>
+              <input
+                type="text"
+                className="input"
+                name="salaryMin"
+                placeholder="Minimum Salary"
+              />
+            </div>
+
+            <div className="">
+              <label className="label">Maximum Salary</label>
+              <input
+                type="text"
+                className="input"
+                placeholder="Maximum Salary"
+                name="salaryMax"
+              />
+            </div>
+
+            <div className="">
+              <label className="label">Currency</label>
+              <select defaultValue="Select a Currency" className="select">
+                <option disabled={true}>Job Category</option>
+                <option>BDT</option>
+                <option>USD</option>
+                <option>EU</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        {/*  */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Salary Range</legend>
+        </fieldset>
+
+        {/*  */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
+        </fieldset>
+
+        {/*  */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
+        </fieldset>
       </form>
     </div>
   );
